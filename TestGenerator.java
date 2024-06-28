e7dee8b23aed0729c45a0a833dab7608ee57051b
Leonard Collins <leonardl@taal.com>
2024-06-28 15:06:54 +0700

    added distinct batches

--- TestGenerator.java
+++ TestGenerator.java
@@ -7,38 +7,47 @@ import java.util.Random;
 
 public class TestGenerator {
     public static void main(String[] args) {
-        // Number of rows to generate
-        int numRows = 100;  // You can change this to any number of rows you want
-
+        // Number of batches to generate
+        int numBatches = 10;  // You can change this to any number of batches you want
+        
         // Filename
         String filename = "output.WIS";
-
+        
         // Generate random number data
         Random random = new Random();
-
-        // Get current timestamp
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm'UTC'");
-        String timestamp = dateFormat.format(new Date());
-
+        
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            // Write the timestamp to the beginning of the file
-            writer.write("Timestamp: " + timestamp);
-            writer.newLine();
-            
-            for (int i = 0; i < numRows; i++) {
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < 8; j++) {
-                    int randomNumber = random.nextInt(1000);  // Generate a random number (0-999)
-                    sb.append(randomNumber);
-                    if (j < 7) {
-                        sb.append("\t");  // Add tab delimiter
+            for (int batch = 0; batch < numBatches; batch++) {
+                // Get current timestamp for the batch
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm'UTC'");
+                String timestamp = dateFormat.format(new Date());
+                
+                // Write the timestamp for the batch
+                writer.write("Timestamp: " + timestamp);
+                writer.newLine();
+                
+                // Random number of rows between 5 and 10 for this batch
+                int numRows = 5 + random.nextInt(6);  // Random number between 5 and 10
+                
+                for (int i = 0; i < numRows; i++) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < 8; j++) {
+                        int randomNumber = random.nextInt(1000);  // Generate a random number (0-999)
+                        sb.append(randomNumber);
+                        if (j < 7) {
+                            sb.append("\t");  // Add tab delimiter
+                        }
                     }
+                    writer.write(sb.toString());
+                    writer.newLine();  // Move to the next line
                 }
-                writer.write(sb.toString());
-                writer.newLine();  // Move to the next line
+                
+                writer.newLine();  // Add an extra line between batches
+                // Simulate a delay for next batch to have a different timestamp
+                Thread.sleep(1000);  // Sleep for 1 second
             }
             System.out.println("Random number data written to " + filename);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             System.err.println("Error writing to file: " + e.getMessage());
         }
     }
